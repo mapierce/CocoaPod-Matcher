e011598a4fde7c0d830e20648b5e9ce3ab353224
Matthew Pierce <mpierce@currencyfair.com>
2020-01-27 11:17:46 +0000

    Now mapping the pods and versions

--- main.py
+++ main.py
@@ -12,8 +12,16 @@ def read_podfile_lock():
 	podfile_lock = open(os.path.dirname(os.path.abspath( __file__ ))+constants.PODFILE_LOCK, "r").readlines()
 	dependencies_index = podfile_lock.index(constants.DEPENDENCIES_STRING)
 	valid_pods = list(filter(lambda x: x.startswith(constants.INSTALLED_POD_PREFIX), podfile_lock[0:dependencies_index])) 
-	for line in valid_pods:	
-		print(line)
+	pods = dict(map(lambda x: split_text_to_pod_and_version(x), valid_pods))
+	for key, val in pods.items():
+		print(key, val)
+
+def split_text_to_pod_and_version(text):
+	items = text.split("(")
+	pod_name = items[0].strip()[2:]
+	pod_version = items[1].strip().split(")")[0]
+	return pod_name, pod_version
+	# print(pod_name + ": " + pod_version)
 
 def read_arguments(args):
 	if len(args) == 2:
